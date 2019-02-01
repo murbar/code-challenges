3558b7d0b2e636d33e61452547fe5eb82733d1a6
Joel Bartlett <joel@murbar.com>
2019-02-01 00:51:37 -0500

    Add tests

--- operators.js
+++ operators.js
@@ -21,9 +21,10 @@ function absVal(num) {
 }
 
 function negCheck(num1, num2) {
-  let neg = false;
-  if ((num1 < 0 || num2 < 0) && !(num1 < 0 && num2 < 0)) neg = true;
-  return [neg, absVal(num1), abcVal(num2)];
+  const neg = (num1 < 0 || num2 < 0) && !(num1 < 0 && num2 < 0);
+  //   let neg = false;
+  //   if ((num1 < 0 || num2 < 0) && !(num1 < 0 && num2 < 0)) neg = true;
+  return [neg, absVal(num1), absVal(num2)];
 }
 
 function multiply(x, y) {
@@ -55,23 +56,28 @@ function modulo(x, y) {
   return mod;
 }
 
-console.log(negCheck(12, 34)); //   <--- [ false, 12, 34 ]
-console.log(negCheck(-12, 34)); //  <--- [ true, 12, 34 ]
-console.log(negCheck(12, -34)); //  <--- [ true, 12, 34 ]
-console.log(negCheck(-12, -34)); // <--- [ false, 12, 34 ]
-console.log(multiply(3, 4)); //     <--- 12
-console.log(multiply(-3, 4)); //    <--- -12
-console.log(multiply(3, -4)); //    <--- -12
-console.log(multiply(-3, -4)); //   <--- 12
-console.log(divide(10, 3)); //      <--- 3
-console.log(divide(-10, 3)); //     <--- -3
-console.log(divide(10, -3)); //     <--- -3
-console.log(divide(-10, -3)); //    <--- 3
-console.log(modulo(10, 3)); //      <--- 1
-console.log(modulo(-10, 3)); //     <--- -1
-console.log(modulo(10, -3)); //     <--- 1
-console.log(modulo(-10, -3)); //    <--- -1
-
-console.log(absVal(-33)); // <- 33
-console.log(absVal(0)); // <- 0
-console.log(absVal(11)); // <- 11
+const tests = [
+  [negCheck(12, 34).toString(), 'false,12,34'],
+  [negCheck(-12, 34).toString(), 'true,12,34'],
+  [negCheck(12, -34).toString(), 'true,12,34'],
+  [negCheck(-12, -34).toString(), 'false,12,34'],
+  [multiply(3, 4), 12],
+  [multiply(-3, 4), -12],
+  [multiply(3, -4), -12],
+  [multiply(-3, -4), 12],
+  [divide(10, 3), 3],
+  [divide(-10, 3), -3],
+  [divide(10, -3), -3],
+  [divide(-10, -3), 3],
+  [modulo(10, 3), 1],
+  [modulo(-10, 3), -1],
+  [modulo(10, -3), 1],
+  [modulo(-10, -3), -1],
+  [absVal(-33), 33],
+  [absVal(0), 0],
+  [absVal(11), 11]
+];
+tests.forEach((t, i) => {
+  const status = t[0] == t[1] ? `passed` : `FAILED`;
+  console.log(`Test ${i} ${status}, output: ${t[0]}, expected: ${t[1]}`);
+});
