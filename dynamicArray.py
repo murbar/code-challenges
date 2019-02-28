d811f534e7d14573518f74f14cd6f4b0b70342e7
Joel Bartlett <joel@murbar.com>
2019-02-28 18:17:31 -0500

    Add dynamicArray

---
+++ dynamicArray.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/dynamic-array/problem
+
+
+def dynamicArray(n, queries):
+    pass
+
+
+# tests
+test_n = 2
+test_queries = [[1, 0, 5], [1, 1, 7], [1, 0, 3], [2, 1, 0], [2, 1, 1]]
