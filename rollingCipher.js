80f7410d5defabd3361db98b3f661e63520ba65d
Joel Bartlett <joel@murbar.com>
2019-03-28 19:24:23 -0400

    Add tests

--- rollingCipher.js
+++ rollingCipher.js
@@ -15,3 +15,20 @@ function rollingCipher(string, num) {
 }
 
 console.log(rollingCipher('abcd', 55));
+
+//Make a cipher: write a function that accepts a string and a number (n) and returns a cipher by rolling each character forward (n > 0) or backward (n < 0) n times.
+
+//Examples:
+rollingCipher('abcd', 1); //"bcde"
+
+rollingCipher('abcd', -1); //"zabc"
+
+rollingCipher('abcd', 3); //"defg"
+
+rollingCipher('abcd', 26); //"abcd"
+
+// Notes:
+// -All letters are lower cased.
+// -Expect no spacing.
+// -Each character is rotated the same number of times.
+// -Think of the letters as a connected loop, so rolling a backwards once will yield z, and rolling z forward once will yield a. If you roll 26 times in either direction, you should end up back where you started.
