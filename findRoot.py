e2dc1a4a6bbd84ef088347b90ced6526d6409190
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-09-17 21:29:13 -0400

    Add findRoot

---
+++ findRoot.py
@@ -0,0 +1,42 @@
+# Root of Number - https://www.pramp.com/challenge/jKoA5GAVy9Sr9jGBjzN4
+# Many times, we need to re-implement basic functions without using any standard library functions already implemented. For example, when designing a chip that requires very little memory space.
+
+# In this question we’ll implement a function root that calculates the n’th root of a number. The function takes a nonnegative number x and a positive integer n, and returns the positive n’th root of x within an error of 0.001 (i.e. suppose the real root is y, then the error is: | y-root(x, n) | and must satisfy | y-root(x, n) | < 0.001).
+
+# Don’t be intimidated by the question. While there are many algorithms to calculate roots that require prior knowledge in numerical analysis(some of them are mentioned here), there is also an elementary method which doesn’t require more than guessing-and-checking. Try to think more in terms of the latter.
+
+# Make sure your algorithm is efficient, and analyze its time and space complexities.
+
+# Examples:
+
+# input:  x = 7, n = 3
+# output: 1.913
+
+# input:  x = 9, n = 2
+# output: 3
+
+# use binary search
+
+
+def root(x, n):
+    if x == 0:
+        return 0
+
+    epsilon = 0.001
+    lowerBound = 0
+    upperBound = x
+    guess = (upperBound - lowerBound) / 2
+
+    while guess - lowerBound >= epsilon:
+        if guess ** n > x:
+            upperBound = guess
+        else:
+            lowerBound = guess
+
+        guess = (upperBound + lowerBound) / 2
+
+    return guess
+
+
+print(root(7, 3))
+print(root(3, 2))
