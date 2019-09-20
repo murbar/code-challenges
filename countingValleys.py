b915d7464cd910492ff61075f76c4c942a853c1d
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-09-19 23:03:30 -0400

    Solve counting valleys

--- countingValleys.py
+++ countingValleys.py
@@ -8,13 +8,21 @@
 # reset highest, lowest after direction change
 
 
-def countingValleys(n, s):
-    pass
+def countingValleys(s):
+    elevation = 0
+    valleys = 0
+    for step in s:
+        is_downhill = step == 'D'
+        is_sea_level = elevation == 0
+        if is_downhill and is_sea_level:
+            valleys += 1
+        elevation = elevation - 1 if is_downhill else elevation + 1
+    return valleys
 
 
 s = "UDDDUDUU"
 desired_result = 1
 
-output = countingValleys(0, s)
+output = countingValleys(s)
 print(output)
 print("Passes:", output == desired_result)
