e77e4b376eafb4b6ec5c75fdfdcad9bd4462cf34
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-12-27 14:45:50 -0500

    Add solution

---
+++ leetcode/sum-two-ints.py
@@ -0,0 +1,61 @@
+# def add(a, b):
+#     if a < 0 and b < 0:
+#         a, b = negate(a), negate(b)
+#         return negate(add(a, b))
+
+#     if a < 0:
+#         return subtract(b, negate(a))
+
+#     if b < 0:
+#         return subtract(a, negate(b))
+
+#     while b != 0:
+#         carry = a & b
+#         a = a ^ b
+#         b = carry << 1
+
+#     return a
+
+
+# def negate(n):
+#     return add(~n, 1)
+
+
+# def subtract(a, b):
+#     negative = True if b > a else False
+#     if negative:
+#         a, b = b, a
+
+#     while b != 0:
+#         borrow = (~a) & b
+#         a = a ^ b
+#         b = borrow << 1
+
+#     return add(~a, 1) if negative else a
+
+
+# not my solution, need to study utility of the mask
+def sum(a, b):
+    mask = 0xffffffff
+
+    while b & mask:
+        carry = a & b
+        a = a ^ b
+        b = carry << 1
+
+    return (a & mask) if b > mask else a
+
+
+# assert add(12, 45) == 57
+# assert add(-12, -45) == -57
+# assert add(12, -45) == -33
+# assert add(-12, 45) == 33
+# assert add(45, -12) == 33
+
+assert sum(12, 45) == 57
+assert sum(-12, -45) == -57
+assert sum(12, -45) == -33
+assert sum(-12, 45) == 33
+assert sum(45, -12) == 33
+# assert subtract(45, 12) == 33
+# assert subtract(12, 45) == -33
