781826483f7906e27edfddfe42d6a47560d60d48
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-12-30 14:04:39 -0500

    Add solution

---
+++ leetcode/best-time-buy-sell.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+
+def maxProfit(prices) -> int:
+    if len(prices) < 2:
+        return 0
+
+    min_price = prices[0]
+    max_profit = prices[1] - min_price
+
+    for i in range(1, len(prices)):
+        current_price = prices[i]
+        potential_profit = current_price - min_price
+        max_profit = max(potential_profit, max_profit)
+        min_price = min(current_price, min_price)
+
+    # no transaction if profit is negative
+    # otherwise just return max_profit
+    return max(max_profit, 0)
+
+
+assert maxProfit([7, 1, 5, 3, 6, 4]) == 5
+assert maxProfit([7, 6, 4, 3, 1]) == 0
+assert maxProfit([]) == 0
+assert maxProfit([5]) == 0
