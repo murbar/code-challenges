796de482572fa84d663748ae2f29138560a30914
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-01-01 15:22:35 -0500

    Add solution

---
+++ leetcode/reverse-string.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/reverse-string/
+
+# string given as list of chars
+# reverse in-place, allocating no extra memory
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
