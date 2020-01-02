2c9eda8341bf39a02bccda853ee1d47361622fcb
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-01-01 21:19:01 -0500

    Add solution

--- leetcode/valid-palindrome.py
+++ leetcode/valid-palindrome.py
@@ -16,3 +16,41 @@ class Solution:
             j -= 1
 
         return True
+
+
+# https://leetcode.com/problems/valid-palindrome-ii/
+# is it a palindrome with any one letter removed?
+class Solution2:
+    def validPalindrome(self, s: str) -> bool:
+
+        def helper(word, error=False):
+            if not len(word):
+                return True
+
+            i, j = 0, len(word) - 1
+            while i < j:
+                if word[i] != word[j]:
+                    if error:
+                        return False
+                    else:
+                        without_i = s[:i] + s[i+1:]
+                        without_j = s[:j] + s[j+1:]
+                        return helper(without_i, True) or helper(without_j, True)
+                i += 1
+                j -= 1
+
+            return True
+
+        return helper(s)
+
+    # not my solution
+    def validPalindrome2(self, s):
+        def is_pali_range(i, j):
+            return all(s[k] == s[j-k+i] for k in range(i, j))
+
+        for i in range(len(s) / 2):
+            # ~1 == -(i+1)
+            if s[i] != s[~i]:
+                j = len(s) - 1 - i
+                return is_pali_range(i+1, j) or is_pali_range(i, j-1)
+        return True
