f22050333cc33309cbf1a5ff60155b07c7c0365d
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-01-03 16:33:17 -0500

    Add solution

---
+++ leetcode/reverse-linked-list.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+# three pointers: one for the current node, one ofr previous, one for next
+# move pointers in specific order to reverse list
+# for each node
+#   record its current next
+#   change its next to the node that pointed to it
+#   move to next node
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        # end of list, None/null
+        preceding = None
+        current = head
+        following = head
+
+        while current != None:
+            # keep reference to next node
+            following = following.next
+            # point current node to previous node
+            current.next = preceding
+            # set previous node to current for next iteration
+            preceding = current
+            # process next node
+            current = following
+
+        return preceding
