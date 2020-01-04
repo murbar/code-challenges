c65fd40c12c922495b4a8ad84850c2fd1546b3ad
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-01-04 15:14:01 -0500

    Add sieve implementation

--- misc/isPrimeNumber.py
+++ misc/isPrimeNumber.py
@@ -24,6 +24,23 @@ def isPrime2(number):
     return number >= 2
 
 
+def isPrime3(number):
+    # using sieve of Eratosthenes
+    # faster, but using memory to store potentially large bit vector
+    if number < 2:
+        return False
+
+    vector = [True] * (number + 1)
+    p = 2
+    while p * p <= number:
+        if vector[p]:
+            for i in range(p * p, number + 1, p):
+                vector[i] = False
+        p += 1
+
+    return vector[-1]
+
+
 assert isPrime(0) == False
 assert isPrime(1) == False
 assert isPrime(2) == True
@@ -41,3 +58,13 @@ assert isPrime2(7) == True
 assert isPrime2(9) == False
 assert isPrime2(435375373415465733) == False
 assert isPrime2(87178291199) == True
+
+assert isPrime3(0) == False
+assert isPrime3(1) == False
+assert isPrime3(2) == True
+assert isPrime3(3) == True
+assert isPrime3(7) == True
+assert isPrime3(9) == False
+# will crash - too much memory!
+# assert isPrime3(435375373415465733) == False
+# assert isPrime3(87178291199) == True
