f83f24bdabe5dd107841b2ac5c068d008a6b0f81
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-01-04 14:13:01 -0500

    Add alt implementation

--- misc/isPrimeNumber.py
+++ misc/isPrimeNumber.py
@@ -10,7 +10,34 @@ def isPrime(number):
     return number >= 2
 
 
-print(isPrime(3))
-print(isPrime(7))
-print(isPrime(9))
-print(isPrime(435375373415465733))
+def isPrime2(number):
+    # here we avoid using any external functions
+    # i * i <= number equivalent to i <= sqrt(num)
+
+    i = 2
+    while i * i <= number:
+        if number % i == 0:
+            return False
+        i += 1
+
+    # excludes 0 and 1
+    return number >= 2
+
+
+assert isPrime(0) == False
+assert isPrime(1) == False
+assert isPrime(2) == True
+assert isPrime(3) == True
+assert isPrime(7) == True
+assert isPrime(9) == False
+assert isPrime(435375373415465733) == False
+assert isPrime(87178291199) == True
+
+assert isPrime2(0) == False
+assert isPrime2(1) == False
+assert isPrime2(2) == True
+assert isPrime2(3) == True
+assert isPrime2(7) == True
+assert isPrime2(9) == False
+assert isPrime2(435375373415465733) == False
+assert isPrime2(87178291199) == True
