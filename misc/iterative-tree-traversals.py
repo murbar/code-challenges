f90078a0dd87654d01f05781e626e8aac5642925
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-01-16 13:50:40 -0500

    Add BF and tests

--- misc/iterative-tree-traversals.py
+++ misc/iterative-tree-traversals.py
@@ -1,5 +1,18 @@
 from typing import List
 
+'''
+     DFS                 DFS                  DFS                 BFS
+ Post-order           Pre-order             In-order
+Bottom -> Top       Top -> Bottom    Left -> Root -> Right    Left -> Right
+Left -> Right       Left -> Right                             Top -> Bottom
+
+      5                   1                    4                   1
+     / \                 / \                  / \                 / \
+    3   4               2   5                2   5               2   3
+   / \                 / \                  / \                 / \
+  1   2               3   4                1   3               4   5
+'''
+
 
 class TreeNode:
     def __init__(self, x):
@@ -11,7 +24,7 @@ class TreeNode:
 # eg. pre-order (root-left-right) is added right-left-root
 
 
-def in_order_traversal(self, root: TreeNode) -> List[int]:
+def in_order_traversal(root: TreeNode) -> List[int]:
     stack = [root]
     res = []
     while stack:
@@ -26,7 +39,7 @@ def in_order_traversal(self, root: TreeNode) -> List[int]:
     return res
 
 
-def post_order_traversal(self, root: TreeNode) -> List[int]:
+def post_order_traversal(root: TreeNode) -> List[int]:
     stack = [root]
     res = []
     while stack:
@@ -41,7 +54,7 @@ def post_order_traversal(self, root: TreeNode) -> List[int]:
     return res
 
 
-def pre_order_traversal(self, root: TreeNode) -> List[int]:
+def pre_order_traversal(root: TreeNode) -> List[int]:
     stack = [root]
     res = []
     while stack:
@@ -57,7 +70,7 @@ def pre_order_traversal(self, root: TreeNode) -> List[int]:
 
 
 # can be done with a flag too, like this:
-def pre_order_traversal2(self, root: TreeNode) -> List[int]:
+def pre_order_traversal2(root: TreeNode) -> List[int]:
     # is this item on the stack a node or a value?
     # (is_node, node/value)
     stack = [(True, root)]
@@ -72,3 +85,39 @@ def pre_order_traversal2(self, root: TreeNode) -> List[int]:
             else:
                 res.append(current)
     return res
+
+
+# BFS with queue
+def bf_traversal(root: TreeNode) -> List[int]:
+    queue = [root]
+    res = []
+    while queue:
+        current = queue.pop(0)
+        if current:
+            if isinstance(current, TreeNode):
+                queue.append(current.val)
+                queue.append(current.left)
+                queue.append(current.right)
+            else:
+                res.append(current)
+    return res
+
+
+# tests
+#     1
+#    / \
+#   2   3
+#  / \
+# 4   5
+root = TreeNode(1)
+root.right = TreeNode(3)
+l = TreeNode(2)
+l.left = TreeNode(4)
+l.right = TreeNode(5)
+root.left = l
+
+assert in_order_traversal(root) == [4, 2, 5, 1, 3]
+assert post_order_traversal(root) == [4, 5, 2, 3, 1]
+assert pre_order_traversal(root) == [1, 2, 4, 5, 3]
+assert pre_order_traversal2(root) == [1, 2, 4, 5, 3]
+assert bf_traversal(root) == [1, 2, 3, 4, 5]
