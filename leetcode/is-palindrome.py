c7aec379b0a5db9501e63d4486e619094be1170e
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-01-20 16:05:18 -0500

    Add solution and tests

--- leetcode/is-palindrome.py
+++ leetcode/is-palindrome.py
@@ -1,13 +1,11 @@
 # https://leetcode.com/problems/valid-palindrome/
 
 # problem seems to be broken on leetcode
+# edge case: is an empty string a palindrome?
 
 
-def isPalindrome(s):
+def is_palindrome(s):
     chars = [c.lower() for c in s if c.isalpha()]
-
-    if not len(chars):
-        return True
 
     i, j = 0, len(chars) - 1
     while i < j:
@@ -19,6 +17,35 @@ def isPalindrome(s):
     return True
 
 
-assert isPalindrome("A man, a plan, a canal: Panama")
-assert isPalindrome("")
-assert not isPalindrome("race a car")
+def is_palindrome2(s):
+    chars = [c.lower() for c in s if c.isalpha()]
+    return all(chars[i] == chars[~i] for i in range(len(chars) // 2))
+
+
+assert not is_palindrome('asdf')
+assert not is_palindrome('abaa')
+assert not is_palindrome('abbaa')
+assert not is_palindrome('ab')
+assert is_palindrome('aa')
+assert is_palindrome('a')
+assert is_palindrome('')
+assert is_palindrome('aca')
+assert is_palindrome('aabbaa')
+assert is_palindrome('aabcbaa')
+assert is_palindrome("A man, a plan, a canal: Panama")
+assert is_palindrome("")
+assert not is_palindrome("race a car")
+
+assert not is_palindrome2('asdf')
+assert not is_palindrome2('abaa')
+assert not is_palindrome2('abbaa')
+assert not is_palindrome2('ab')
+assert is_palindrome2('aa')
+assert is_palindrome2('a')
+assert is_palindrome2('')
+assert is_palindrome2('aca')
+assert is_palindrome2('aabbaa')
+assert is_palindrome2('aabcbaa')
+assert is_palindrome2("A man, a plan, a canal: Panama")
+assert is_palindrome2("")
+assert not is_palindrome2("race a car")
