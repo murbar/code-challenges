923a81d7ce2378bae7f589a85c6d22ed93db146a
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-01-31 13:38:40 -0500

    Add recursive solution

--- leetcode/find-peak-element.py
+++ leetcode/find-peak-element.py
@@ -1,12 +1,14 @@
 # https://leetcode.com/problems/find-peak-element/
 
 # all elements preceding a peak are sorted smallest -> largest
-# use binary search to find any peak in the array
+# nums[i] and nums[i+1] are never equal
+# use binary search to find "any peak"in the array
 
 from typing import List
 
 
 class Solution:
+    # O(lg n) time, O(1) space
     def findPeakElement(self, nums: List[int]) -> int:
         left = 0
         right = len(nums) - 1
@@ -21,6 +23,25 @@ class Solution:
         return left
 
 
+class SolutionRecursive:
+    # O(lg n) time and space (extra space for call stack)
+    def search(self, nums, left, right):
+        if left == right:
+            return left
+
+        mid = (left + right) // 2
+        if nums[mid] < nums[mid+1]:
+            return self.search(nums, mid+1, right)
+
+        return self.search(nums, left, mid)
+
+    def findPeakElement(self, nums: List[int]) -> int:
+        return self.search(nums, 0, len(nums)-1)
+
+
 s = Solution()
+sr = SolutionRecursive()
 assert s.findPeakElement([1, 2, 1, 3, 5, 6, 4]) == 1 or s.findPeakElement(
+    [1, 2, 1, 3, 5, 6, 4]) == 5
+assert sr.findPeakElement([1, 2, 1, 3, 5, 6, 4]) == 1 or sr.findPeakElement(
     [1, 2, 1, 3, 5, 6, 4]) == 5
